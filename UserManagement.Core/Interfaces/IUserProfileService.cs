0d7be49e4fa5723b3d32e6816cd881b22f1d7c4c
Onyinyechi Onyenaucheya <preciousonyenaucheya10@gmail.com>
2022-10-25 22:26:54 +0100

    Commit

---
+++ UserManagement.Core/Interfaces/IUserProfileService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using UserManagement.Core.DTOs;
+using UserManagement.Domain.Models;
+
+namespace UserManagement.Core.Interfaces
+{
+    public interface IUserProfileService
+    {
+        Task<ResponseDto<string>> UploadImageAsync(string id, IFormFile file);
+        Task<ResponseDto<GetProfileDTO>> GetUserProfile(string Id);
+        Task<ResponseDto<GetUserDTO>> GetUserById(string id);
+    }
+}
